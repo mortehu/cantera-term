f5b41236e490944a1c50229866a8334e1439f846
Morten Hustveit <morten.hustveit@gmail.com>
2013-01-24 10:10:33 -0800

    Remove references to image libraries and Xdamage
    These libraries are no longer being used for anything.
    
    Signed-off-by: Morten Hustveit <morten.hustveit@gmail.com>

--- term.c
+++ term.c
@@ -33,8 +33,6 @@
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 #include <X11/cursorfont.h>
-#include <X11/extensions/Xdamage.h>
-#include <X11/extensions/Xinerama.h>
 #include <X11/extensions/Xrender.h>
 #include <X11/keysym.h>
 
@@ -137,8 +135,6 @@ const struct
 };
 
 struct terminal terminal;
-int damage_eventbase;
-int damage_errorbase;
 int screenidx;
 int cols;
 int rows;
