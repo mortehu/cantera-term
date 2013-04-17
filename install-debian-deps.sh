48a4980b88aedd614815ba3fc753a25ab6985713
Morten Hustveit <morten.hustveit@gmail.com>
2013-04-17 09:35:37 -0700

    Add libglew-dev to install-debian-deps.sh
    Signed-off-by: Morten Hustveit <morten.hustveit@gmail.com>

--- install-debian-deps.sh
+++ install-debian-deps.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 
-apt-get install build-essential libfreetype6-dev libxrender-dev libxxf86vm-dev libglu1-mesa-dev libxft-dev
+apt-get install build-essential libfreetype6-dev libxrender-dev libxxf86vm-dev libglu1-mesa-dev libxft-dev libglew-dev
