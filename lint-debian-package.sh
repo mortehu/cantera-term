65b96546929d2eb58b38aeab39cddec2589883ca
Morten Hustveit <morten.hustveit@gmail.com>
2014-03-09 15:34:27 -0700

    Fixed Debian package and added lintian test
    Signed-off-by: Morten Hustveit <morten.hustveit@gmail.com>

---
+++ lint-debian-package.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Exit if a command fails
+set -e
+
+unset DEB_BUILD_OPTIONS
+
+make dist
+
+SRCDIR=$(pwd)
+WORKDIR=$(mktemp -d)
+trap "rm -rf $WORKDIR" EXIT
+
+cd "$WORKDIR"
+
+mv "$SRCDIR"/cantera-term-31.tar.gz cantera-term_31.orig.tar.gz
+tar zxpvf cantera-term_31.orig.tar.gz
+cd cantera-term-31
+cp -va "$SRCDIR"/debian .
+dpkg-buildpackage -us -uc -rfakeroot
+cd ..
+lintian --fail-on-warnings --suppress-tags new-package-should-close-itp-bug *.changes
